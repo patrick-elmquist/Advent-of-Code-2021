9905011a88055c1d5cd80d35202f8895670646bb
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-12-06 10:31:17 +0100

    Added asserts for safeRange

--- src/main/kotlin/util/extensions/NumberExtensions.kt
+++ src/main/kotlin/util/extensions/NumberExtensions.kt
@@ -2,9 +2,12 @@ package util.extensions
 
 fun main() {
     // isBitSet tests
-    check(0b101.isBitSet(0)) { "Index 0 didn't work" }
-    check(!0b101.isBitSet(1)) { "Index 1 didn't work" }
-    check(0b101.isBitSet(2)) { "Index 1 didn't work" }
+    0b101.isBitSet(0) assert true
+    0b101.isBitSet(1) assert false
+    0b101.isBitSet(2) assert true
+
+    (1 safeRange 5) assert (1 .. 5)
+    (5 safeRange 1) assert (5 downTo 1)
 
     println("Test OK")
 }
@@ -17,3 +20,5 @@ fun Int.getBit(index: Int): Int = if (isBitSet(index)) 1 else 0
  * Creates a range of the two points, automatically handling the direction
  */
 infix fun Int.safeRange(other: Int) = if (this < other) this..other else this downTo other
+
+private infix fun <T> T.assert(expected: T) = assert(this == expected) { "Assert failed: $this != $expected" }
