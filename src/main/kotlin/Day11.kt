5b81e8a739de9dbf2e00e720eb2ae5f8c82023f3
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-12-12 23:47:20 +0100

    More cleanup of Day 11

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,3 +1,4 @@
+import util.Input
 import util.Point
 import util.day
 import util.neighbors
@@ -5,36 +6,32 @@ import util.neighbors
 // answer #1: 1773
 // answer #2: 494
 
+private typealias Matrix = MutableMap<Point, Int>
+
 fun main() {
     day(n = 11) {
         solution(expected = 1773) { input ->
-            val matrix = input.lines
-                .flatMapIndexed { y, row -> row.mapIndexed { x, c -> Point(x, y) to c.digitToInt() } }
-                .toMap()
-                .toMutableMap()
-
-            generateSequence { matrix.step() }.take(100).sum()
+            val matrix = input.parseMatrix()
+            generateSequence { matrix.nextStep() }.take(100).sum()
         }
 
         solution(expected = 494) { input ->
-            val matrix = input.lines
-                .flatMapIndexed { y, row -> row.mapIndexed { x, c -> Point(x, y) to c.digitToInt() } }
-                .toMap()
-                .toMutableMap()
-
-            1 + generateSequence { matrix.step().takeUnless { matrix.isAllZeros() } }.count()
+            val matrix = input.parseMatrix()
+            1 + generateSequence { matrix.nextStep().takeUnless { matrix.isAllZeros() } }.count()
         }
     }
 }
 
-private fun MutableMap<Point, Int>.step(): Int = incrementLevels().executeFlashes().resetFlashed()
+private fun Input.parseMatrix() =
+    lines.flatMapIndexed { y, row -> row.mapIndexed { x, c -> Point(x, y) to c.digitToInt() } }
+        .toMap()
+        .toMutableMap()
 
-private fun MutableMap<Point, Int>.isAllZeros(): Boolean = all { it.value == 0 }
+private fun Matrix.nextStep() = incrementLevels().executeFlashes().resetFlashed()
 
-private fun MutableMap<Point, Int>.incrementLevels(): MutableMap<Point, Int> =
-    onEach { set(it.key, getValue(it.key) + 1) }
+private fun Matrix.incrementLevels() = onEach { set(it.key, getValue(it.key) + 1) }
 
-private fun MutableMap<Point, Int>.executeFlashes(): MutableMap<Point, Int> {
+private fun Matrix.executeFlashes(): Matrix {
     val visited = mutableSetOf<Point>()
     val queue = filter { it.value > 9 }.map { it.key }.toMutableList()
     while (queue.isNotEmpty()) {
@@ -52,4 +49,6 @@ private fun MutableMap<Point, Int>.executeFlashes(): MutableMap<Point, Int> {
     return this
 }
 
-private fun MutableMap<Point, Int>.resetFlashed(): Int = filter { it.value > 9 }.onEach { set(it.key, 0) }.count()
+private fun Matrix.resetFlashed() = filter { it.value > 9 }.onEach { set(it.key, 0) }.count()
+
+private fun Matrix.isAllZeros() = all { it.value == 0 }
