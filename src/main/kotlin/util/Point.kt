28a596833dc312a168a1698b6ef52fd4d2ea13a0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-12-13 00:00:45 +0100

    Fixed Point class

--- src/main/kotlin/util/Point.kt
+++ src/main/kotlin/util/Point.kt
@@ -4,15 +4,15 @@ data class Point(val x: Int, val y: Int)
 
 fun Point.neighbors(diagonal: Boolean = false): List<Point> =
     buildList {
-        this += copy(x = x - 1)
-        this += copy(x = x + 1)
-        this += copy(y = y - 1)
-        this += copy(y = y + 1)
+        add(copy(x = x - 1))
+        add(copy(x = x + 1))
+        add(copy(y = y - 1))
+        add(copy(y = y + 1))
 
         if (diagonal) {
-            this += Point(x - 1, y - 1)
-            this += Point(x - 1, y + 1)
-            this += Point(x + 1, y - 1)
-            this += Point(x + 1, y + 1)
+            add(Point(x - 1, y - 1))
+            add(Point(x - 1, y + 1))
+            add(Point(x + 1, y - 1))
+            add(Point(x + 1, y + 1))
         }
     }
