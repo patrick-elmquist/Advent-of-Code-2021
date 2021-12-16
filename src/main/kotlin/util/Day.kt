4f6b5062dfde95967b87f750a8b917881a1a5eb5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-12-16 07:57:27 +0100

    Half finished re-write of Day

--- src/main/kotlin/util/Day.kt
+++ src/main/kotlin/util/Day.kt
@@ -5,8 +5,11 @@ package util
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
-import util.AssertStep.Break
-import util.AssertStep.EqualTo
+import util.Assert.Break
+import util.Assert.EqualTo
+import util.Out.fail
+import util.Out.pass
+import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimedValue
 import kotlin.time.measureTimedValue
@@ -36,32 +39,23 @@ private inline fun collectSolutions(block: AnswerSheet.() -> Unit): AnswerSheet 
     AnswerSheet().apply(block)
 
 private suspend fun AnswerSheet.runAssertions(): Result<AnswerSheet> = coroutineScope scope@{
-    val result = solutions.withIndex()
-        .filter { (_, solution) -> solution.asserts.isNotEmpty() }
-        .fold(true) { overallResult, (index, solution) ->
-            println("SOLUTION #${index + 1} ASSERTS")
-            println("===================")
+    val solutionsWithAsserts = solutions.filter { solution -> solution.asserts.isNotEmpty() }
+    val result = solutionsWithAsserts.fold(true) { overallResult, solution ->
+        val (number, _, asserts) = solution
+        println("SOLUTION #${number} ASSERTS")
+        println("===================")
 
-            val (algorithm, asserts) = solution
-            val solutionAsserts = asserts.fold(true) { solutionResult, step ->
-                when (step) {
-                    is Break -> return@scope Result.failure(BreakAssertionException)
-                    is EqualTo -> {
-                        val input = Input(step.input.lines())
-                        val result = algorithm(input)
-                        when {
-                            result == step.expected -> print(pass(input))
-                            failFast ->
-                                return@scope Result.failure(AssertFailedException(fail(input, step.expected, result)))
-                            else -> print(fail(input, step.expected, result))
-                        }
-                        result == step.expected
-                    }
-                } && solutionResult
-            }
-            println()
-            solutionAsserts && overallResult
-        }
+        asserts.map { test -> solution.assert(test) }
+            .all { result ->
+                result.onSuccess {
+                    println(it)
+                }.onFailure {
+                    println(it.message)
+                    if (failFast) return@scope Result.failure(AssertFailedException(it.message ?: "null"))
+                }
+                result.isSuccess
+            } && overallResult
+    }
 
     if (result) {
         Result.success(this@runAssertions)
@@ -70,71 +64,103 @@ private suspend fun AnswerSheet.runAssertions(): Result<AnswerSheet> = coroutine
     }
 }
 
-@OptIn(ExperimentalTime::class)
-private suspend fun AnswerSheet.evaluate(
-    input: Input
-): TimedValue<List<TimedValue<Any?>>> = coroutineScope {
-    measureTimedValue {
-        solutions.map { (algorithm, _, expected) ->
-            measureTimedValue {
-                algorithm(input).also {
-                    if (failFast) {
-                        check(expected == null || expected == it) { "FAIL Expected:$expected got:$it" }
-                    } else if (expected != null && expected != it) {
-                        println("FAIL Expected:$expected got:$it")
-                    }
-                }
+private suspend fun Solution.assert(test: Assert): Result<String> = coroutineScope {
+    when (test) {
+        is Break -> Result.failure(BreakAssertionException)
+        is EqualTo -> {
+            val input = Input(test.input.lines())
+            when (val result = algorithm(input)) {
+                test.expected -> Result.success(pass(input))
+                else -> Result.failure(AssertFailedException(fail(input, test.expected, result)))
             }
         }
     }
 }
 
 @OptIn(ExperimentalTime::class)
-private fun TimedValue<List<TimedValue<Any?>>>.printResults() =
+private suspend fun AnswerSheet.evaluate(
+    input: Input
+): TimedValue<List<Answer>> = coroutineScope {
+    measureTimedValue { solutions.map { evaluateSolution(it, input) } }
+}
+
+@OptIn(ExperimentalTime::class)
+private suspend fun AnswerSheet.evaluateSolution(
+    solution: Solution,
+    input: Input
+): Answer = coroutineScope {
+    val (_, algorithm, _, expected) = solution
+    val result = measureTimedValue {
+        algorithm(input).also {
+            if (failFast) {
+                check(expected == null || expected == it) { "FAIL Expected:$expected got:$it" }
+            } else if (expected != null && expected != it) {
+                println("FAIL Expected:$expected got:$it")
+            }
+        }
+    }
+    Answer(solution.number, result.value, result.duration)
+}
+
+@OptIn(ExperimentalTime::class)
+private fun TimedValue<List<Answer>>.printResults() =
     buildString {
         val (results, totalDuration) = this@printResults
-        results.forEachIndexed { index, (answer, time) ->
-            appendLine("Answer: #${index + 1}: ${answer ?: "Failed"} (${time.inWholeMilliseconds}ms)")
+        results.forEach { (number, output, time) ->
+            appendLine("Answer: #${number}: ${output ?: "Failed"} (${time.inWholeMilliseconds}ms)")
         }
         append("Total duration: ${totalDuration.inWholeMilliseconds}ms")
     }.let { println(it) }
 
-private fun pass(input: Input) =
-    buildString {
-        appendLine("PASS: ${input.lines}")
-    }
+object Out {
+    fun pass(input: Input) =
+        buildString {
+            appendLine("PASS: ${input.lines}")
+        }
 
-private fun fail(input: Input, expected: Any, result: Any?): String =
-    buildString {
-        appendLine("FAIL: ${input.lines}")
-        appendLine("  Expected: $expected Actual: $result")
-    }
+    fun fail(input: Input, expected: Any, result: Any?): String =
+        buildString {
+            appendLine("FAIL: ${input.lines}")
+            appendLine("  Expected: $expected Actual: $result")
+        }
+}
 
 typealias Algorithm = suspend CoroutineScope.(Input) -> Any?
 
 data class Solution(
+    val number: Int,
     val algorithm: Algorithm,
-    val asserts: List<AssertStep>,
+    val asserts: List<Assert>,
     val expected: Any?
+)
+
+private data class Answer(
+    val number: Int,
+    val output: Any?,
+    val time: Duration
 )
 
 class AnswerSheet {
     private val _solutions = mutableListOf<Solution>()
+    private var asserts = mutableListOf<Assert>()
+    private var ignore = false
+    private var solutionCalls = 0
 
     val solutions: List<Solution> = _solutions
-
     var failFast = false
 
-    var ignore = false
-
-    private var asserts = mutableListOf<AssertStep>()
-
     fun solution(expected: Any? = null, algorithm: Algorithm) {
+        solutionCalls++
         if (!ignore) {
-            _solutions.add(Solution(algorithm, asserts.toList(), expected))
+            _solutions.add(Solution(solutionCalls, algorithm, asserts.toList(), expected))
         }
         ignore = false
         asserts.clear()
+    }
+
+    @Suppress("unused")
+    fun ignore() {
+        ignore = true
     }
 
     @Suppress("unused")
@@ -143,9 +169,9 @@ class AnswerSheet {
     infix fun String.assert(result: Any) = asserts.add(EqualTo(this, result))
 }
 
-sealed class AssertStep {
-    object Break : AssertStep()
-    data class EqualTo(val input: String, val expected: Any) : AssertStep()
+sealed class Assert {
+    object Break : Assert()
+    data class EqualTo(val input: String, val expected: Any) : Assert()
 }
 
 private sealed class AnswerException(message: String = "") : Exception(message)
