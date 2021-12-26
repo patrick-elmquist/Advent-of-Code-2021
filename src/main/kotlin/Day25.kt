fda24351d362a80ef3183fe51f524de529580d0a
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-12-26 12:54:17 +0100

    Cleanup of Day 25 Part 1

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,93 +1,46 @@
 
 import util.Point
 import util.day
-import util.log
 
-// answer #1:
+// answer #1: 516
 // answer #2:
 
 fun main() {
     day(n = 25) {
-        """
-v...>>.vv>
-.vv>>.vv..
->>.>v>...v
->>v>>.>.v.
-v>v.vv.v..
->.>>..v...
-.vv..>.>v.
-v.v..>>v.v
-....v..v.>
-""".trimIndent() expect 58
-        part1 { input ->
-            var state = input.lines.flatMapIndexed { y, line ->
+        part1(expected = 516) { input ->
+            val state = input.lines.flatMapIndexed { y, line ->
                 line.mapIndexedNotNull { x, c ->
-                    if (c != '.') {
-                        Point(x, y) to c
-                    } else {
-                        null
-                    }
+                    if (c != '.') Point(x, y) to c else null
                 }
             }.toMap()
 
-            val width = input.lines.first().lastIndex.log("width:")
-            val height = input.lines.lastIndex.log("height:")
+            val width = input.lines.first().length
+            val height = input.lines.size
 
-            var counter = 0
-            while (true) {
-                val movingRight = state
-                    .filter { it.value == '>' }
-                    .mapNotNull { (p, _) ->
-                        var next = p.copy(x = p.x + 1)
-                        if (next.x > width) next = p.copy(x = 0)
-                        if (next !in state) {
-                            p to next
-                        } else {
-                            null
-                        }
-                    }
+            state.sequence(width, height).count()
+        }
+    }
+}
 
-                val newState = state.toMutableMap()
-                movingRight.forEach { (from, to) ->
-                    val c = newState.remove(from)!!
-                    newState[to] = c
-                }
+private fun Map<Point, Char>.sequence(width: Int, height: Int) =
+    generateSequence(this) { state ->
+        val newState = state.toMutableMap()
 
-                val movingDown = newState
-                    .filter { it.value == 'v' }
-                    .mapNotNull { (p, _) ->
-                        var next = p.copy(y = p.y + 1)
-                        if (next.y > height) next = p.copy(y = 0)
-                        if (next !in newState) {
-                            p to next
-                        } else {
-                            null
-                        }
-                    }
-
-                movingDown.forEach { (from, to) ->
-                    val c = newState.remove(from)!!
-                    newState[to] = c
-                }
-
-
-                state = newState
-                counter ++
-                if (movingDown.size + movingRight.size == 0) break
+        val movingRight = state
+            .filter { it.value == '>' }
+            .mapNotNull { (p, _) ->
+                val next = p.copy(x = (p.x + 1) % width)
+                if (next !in state) p to next else null
             }
+        movingRight.forEach { (from, to) -> newState[to] = newState.remove(from)!! }
 
-            counter
-        }
+        val movingDown = newState
+            .filter { it.value == 'v' }
+            .mapNotNull { (p, _) ->
+                val next = p.copy(y = (p.y + 1) % height)
+                if (next !in newState) p to next else null
+            }
+        movingDown.forEach { (from, to) -> newState[to] = newState.remove(from)!! }
+
+        if (movingDown.size + movingRight.size == 0) null else newState
     }
-}
-
-
-private fun Map<Point, Char>.print(width: Int, height: Int) {
-    (0..height).forEach { y ->
-        (0..width).forEach { x ->
-            val c = get(Point(x, y))
-            print("${c ?: '.'} ")
-        }
-        println()
-    }
-}
